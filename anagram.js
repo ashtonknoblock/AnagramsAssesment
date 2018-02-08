e42a7f82a416e78e273f6310d9230d54a1e370c7
Ashton Knoblock <Ashton.knoblock@gmail.com>
2018-02-08 13:29:59 -0500

    first commit

---
+++ anagram.js
@@ -0,0 +1,20 @@
+document.getElementById("findButton").onclick = function() {
+
+    var typedText = document.getElementById("input").value;
+  
+    function alphabetize(a) {
+      return a.toLowerCase().split("").sort().join("").trim();
+    }
+  
+    for (i = 0; i < words.length; i++) {
+      alphabetize(words[i]);
+  
+      if (alphabetize(words[i]) === alphabetize(typedText)) {
+        var span = document.createElement("span");
+        var textContent = document.createTextNode(words[i] + " ");
+        span.appendChild(textContent);
+        document.getElementById("div").appendChild(span);
+      }
+    }
+  }
+  
